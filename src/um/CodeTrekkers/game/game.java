f9600b9f334e6b15deac7f970a6fee3cd9998eaf
nija123098 <nija123098@gmail.com>
2014-09-10 20:22:44 -0500

    initialPreProgramCom commit
    YAY

---
+++ src/um/CodeTrekkers/game/game.java
@@ -0,0 +1,12 @@
+
+package um.CodeTrekkers.game;
+
+/**
+ * Created by nija123098 on 9/1/2014.
+ */
+public class game {
+    public static void game() {
+        //Story Gen
+        //if
+    }
+}
