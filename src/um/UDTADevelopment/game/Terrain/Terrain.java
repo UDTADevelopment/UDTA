fc1902dbdc0b52b7b3f51d64c10603889cc56335
nija123098 <nija123098@gmail.com>
2014-09-27 23:04:54 -0500

    Slight cleaning
    Slight cleaning, more to come.
    Bug which makes all terrain the same type is not fixed.

--- src/um/UDTADevelopment/game/Terrain/Terrain.java
+++ src/um/UDTADevelopment/game/Terrain/Terrain.java
@@ -9,40 +9,20 @@ import um.UDTADevelopment.reference.SettingsKindOf;
 public class Terrain {
 
     public static int mapsize = 10;
-
-    //////////////////////////////////////////////////////////////////////
-    ////////////////////  terrain gen method attempt one.  All the same #/
-    /*
-    public static void TerrainGen(){
-        terrain = new int[mapsize][mapsize];
-        for(int c=0;c<mapsize;c++){
-            for(int o=0;o<mapsize;o++){
-                terrain[c][o] = Rand.Rand(SettingsKindOf.numofbio);
-            }
-        }
-    }
-    ////////////////////
-        public static int TerrainRead(int c1, int c2){
-        return terrain[c1][c2];
-    }
-    */
-    //////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////
-    //
     public static int terrain[][];
 
     public static void TerrainGen(){
         terrain = new int[mapsize][mapsize];
         for(int c=0;c<mapsize;c++){
             for(int o=0;o<mapsize;o++){
-                terrain[c][o] = 0;//set up for possible map function
+                terrain[c][o] = -1;//set up for possible map function
             }
         }
     }
     //
     public static int TerrainRead(int c1, int c2){
-        if (terrain[c1][c2] == 0){
-            terrain[c1][c2] = Rand.Rand(SettingsKindOf.numofbio);
+        if (terrain[c1][c2] == -1){
+            terrain[c1][c2] = Rand.Rand(SettingsKindOf.numofbio + 1);
         }
         return terrain[c1][c2];
     }
